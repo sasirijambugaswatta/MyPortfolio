fb7975572d1c0a3f72127cd2a66f0b825077b1b3
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2024-03-13 10:31:54 +0530

    Complete Nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.module.css'
 import styles from "./App.module.css";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
 
   return (
     <div className={styles.App}>
-        Hello
+        <Navbar />
     </div>
   )
 }
